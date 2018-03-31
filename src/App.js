c1581767c0499c8c877aa1860ee8e68947410328
Yves Freypons <yfreypons@gmail.com>
2018-03-31 08:46:20 -0700

    Add a'Home' route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/pages/Home.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Router>
+          <div>
+            <Route exact path="/" component={Home}/>
+          </div>
+        </Router>
       </div>
     );
   }
